94d52f642aad40ef0473c713da30f9001b7e7ee4
mdsultanrazibuddin <mdsultanrazibuddin@gmail.com>
2024-11-01 11:17:07 +0600

    'added'

---
+++ module1/src/basicDataType.ts
@@ -0,0 +1,38 @@
+// Basic Data Type = primitive data type
+
+// string
+
+let firstName: string = 'Razib'
+
+// Number
+
+let age:number = 26
+
+// Boolean
+ let isAdmin:boolean = true
+
+// undefined
+
+let x:undefined= undefined
+
+//  Null 
+
+let y:null = null
+
+
+// non primitive data type
+
+// array
+let friends : string[] =['rashed', 'jarin']
+
+
+let roll: number[] = [25, 45]
+
+roll.push()
+
+// tuple --> array ---> order ---> types of values
+
+
+let coordinate :[Number, Number] = [23, 23]
+
+let user:[string, number, boolean] = ['sultan', 44, true]
