86c4d86d3967eaa09ef74c14c7f5f84666e7c5f7
mdsultanrazibuddin <mdsultanrazibuddin@gmail.com>
2024-11-01 11:35:37 +0600

    'added'

---
+++ module1/src/objectTypes.ts
@@ -0,0 +1,17 @@
+
+// referance type ---> object
+
+const users:{
+    company: 'abc company' //প্রপার্টি মান ফিক্সড হলে তাকে literal types বলে
+    firstName: string;
+    middleName?: string;   /**প্রপার্টি থাকতেও পারে নাও থাকতে পারে ...তাহলে তাকে optional types বলে */
+    lastname: string;
+    married: boolean;
+} ={
+    company: 'abc company', 
+    firstName: 'razib',
+
+    lastname:'sultan',
+    married: true,
+
+}
